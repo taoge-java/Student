371f13c4e4cbe1ccbdc459ef7d9f9c9bcd66308f
zengjintao <zengjintao@USER-20170420YI>
2017-04-22 20:47:43 +0800

    ..

---
+++ src/main/java/com/student/interceptor/ViewContextInterceptor.java
@@ -0,0 +1,30 @@
+package com.student.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.velocity.tools.generic.DateTool;
+import org.apache.velocity.tools.generic.NumberTool;
+
+import com.jfinal.aop.Interceptor;
+import com.jfinal.aop.Invocation;
+import com.student.constant.CommonConstant;
+import com.student.dao.UserSession;
+/**
+ * @author zengjintao
+ * @version 1.0
+ * @create_at 2017年4月22日 下午5:29:20
+ */
+public class ViewContextInterceptor implements 	Interceptor{
+
+	@Override
+	public void intercept(Invocation inv) {
+        HttpServletRequest request = inv.getController().getRequest();
+		request.setAttribute("dateTool", new DateTool());
+		request.setAttribute("number", new NumberTool());
+		UserSession session=(UserSession) request.getSession().getAttribute(CommonConstant.SESSION_ID_KEY);
+		if(session!=null){
+			request.setAttribute("session", session);
+		}
+		inv.invoke();
+	}
+}
