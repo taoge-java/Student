6ee5a90fbdfb2eea71f6bdd863db01279e0da8c9
zengjintao <zengjintao@USER-20170425TY>
2017-05-09 15:42:26 +0800

    ...

--- src/main/java/com/student/controller/account/ForgetPassword.java
+++ src/main/java/com/student/controller/account/ForgetPassword.java
@@ -1,6 +1,7 @@
 package com.student.controller.account;
 
 import com.jfinal.ext.route.ControllerBind;
+import com.student.common.BaseController;
 /**
  * 忘记密码找回
  * @author zengjintao
@@ -8,6 +9,6 @@ import com.jfinal.ext.route.ControllerBind;
  * @create_at 2017年4月26日 下午9:19:57
  */
 @ControllerBind(controllerKey="/account/forgetPassword")
-public class ForgetPassword {
+public class ForgetPassword extends BaseController{
 
 }
