72493fbfb4e812091534c2a00751e3f22640e00a
zengjintao <1913188966@qq.com>
2017-05-25 22:31:43 +0800

    。。

---
+++ src/main/java/com/student/interceptor/Inject.java
@@ -0,0 +1,22 @@
+package com.student.interceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Inject {
+
+private Inject() {}
+	
+	@Inherited
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD})
+	public static @interface BY_TYPE {}
+	
+	@Inherited
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.FIELD})
+	public static @interface BY_NAME {}
+}
